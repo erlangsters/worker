d5a8e1ef0b9cf1607d39039b5601d3c78df8edac
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-12-17 04:03:01 +0800

    Add LICENSE.md (and copyright headers)

--- src/worker.erl
+++ src/worker.erl
@@ -1,3 +1,12 @@
+%%
+%% Copyright (c) 2024, Byteplug LLC.
+%%
+%% This source file is part of a project made by the Erlangsters community and
+%% is released under the MIT license. Please refer to the LICENSE.md file that
+%% can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, November 2024
+%%
 -module(worker).
 -export([foobar/0]).
 
